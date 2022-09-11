c6d088c1e49d61d78e5adb973b2538510fe6bb3e
Abubakar Rizwan <99861285+Bakar515@users.noreply.github.com>
2022-09-11 11:42:22 +0500

    Bootstrap & jQuery done

---
+++ jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#div1show").click(function() {
+        $("#all").css("display","block");
+    });
+});
+$(document).ready(function() {
+    $("#div2show").click(function() {
+        $("#all").css("display","none");
+        $("#camera").css("display","block");
+    });
+});
